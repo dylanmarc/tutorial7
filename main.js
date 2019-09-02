fed5947dfe109c74bd2e6f3f7140daefc1f1351b
EmilSchroder <eschro66@gmail.com>
2019-09-02 12:36:27 +1200

    tut 7 code

---
+++ main.js
@@ -0,0 +1,19 @@
+function changeColor(){
+
+}
+
+function createAlert(){
+
+}
+
+function crossOff(){
+
+}
+
+function printArray(){
+    let arrayToPrint = ['Snake', 'Dog' ,'Scorpion', 'Crocodile', 'Hippopotamus']
+}
+
+function recordClicks(){
+    
+}
